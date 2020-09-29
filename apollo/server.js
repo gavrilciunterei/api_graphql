1686fb0b6322602b397a7a27be4a8ba221986532
gavrilciunterei <gavriltaller@gmail.com>
2020-09-29 21:35:30 +0300

    pasado todo a apollo

--- apollo/server.js
+++ apollo/server.js
@@ -3,25 +3,27 @@ const { makeExecutableSchema } = require('graphql-tools');
 let courses = require('./courses');
 
 const typeDefs = `
-
-type Course{
+  type Course{
     id: ID!
     title: String!
     views: Int
-}
-
-input CourseInput{
+  }
+  input CourseInput{
     title: String!
     views: Int
   }
-
-type Query{
-  getCourses(page: Int, limit: Int = 1) : [Course]
-}
-
-type Mutation{
+  type Alert{
+    message: String
+  }
+  type Query{
+    getCourses(page: Int, limit: Int = 1) : [Course]
+    getCourse(id: ID!): Course
+  }
+  type Mutation{
     addCourse(input: CourseInput): Course
-}
+    updateCourse(id: ID!, input: CourseInput): Course
+    deleteCourse(id: ID!):Alert
+  }
 `;
 
 const resolvers = {
@@ -32,6 +34,10 @@ const resolvers = {
       }
       return courses;
     },
+    getCourse(obj, { id }) {
+      console.log(id);
+      return courses.find((course) => id == course.id);
+    },
   },
   Mutation: {
     addCourse(obj, { input }) {
@@ -40,6 +46,23 @@ const resolvers = {
       const course = { id, title, views }; // se puede poner sin : ya que tiene el mismo nombre que la prop
       courses.push(course);
       return course;
+    },
+    updateCourse(obj, { id, input }) {
+      // buscamos el curso que queremos modificar
+      const courseIndex = courses.findIndex((course) => id === course.id);
+      const course = courses[courseIndex];
+      // reemplaza los valores inciales del objeto que está actualizando
+      const newCourse = Object.assign(course, { input });
+      // guardamos en el arrya el nuevo para sustituir el viejo
+      course[courseIndex] = newCourse;
+
+      return newCourse;
+    },
+    deleteCourse(obj, { id }) {
+      courses = courses.filter((course) => course.id != id);
+      return {
+        message: `El curso con id ${id} fue eliminado`,
+      };
     },
   },
 };
