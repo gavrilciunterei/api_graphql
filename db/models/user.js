fcc59ce0be6f437d68bc1b600a113d0e6ed5e65b
gavrilciunterei <gavriltaller@gmail.com>
2020-10-02 13:28:08 +0300

    Encriptacion contraseña

--- db/models/user.js
+++ db/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   email: String,
@@ -12,6 +13,18 @@ const userSchema = new mongoose.Schema({
       ref: 'Course',
     },
   ],
+});
+
+userSchema.virtual('password');
+userSchema.pre('validate', async function () {
+  if (this.password === undefined) return;
+  try {
+    const hash = await bcrypt.hash(this.password, 10);
+    this.hashedPassword = hash;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
 });
 
 module.exports = mongoose.model('User', userSchema);
